14f1341fe3e28f340567962c6aa6b77839371581
Amanda Yang <1756843703@qq.com>
2019-01-30 11:16:24 +0800

    init

---
+++ TestDemo/TestDemo/publish.development.sh
@@ -0,0 +1,13 @@
+docker container ls -a | grep "testdemo"
+
+if[ $? -eq 0 ];then
+	docker container stop testdemo
+	docker container rm testdemo
+	docker rmi testdemo
+	docker network ls |grep testdemo
+	docker network rm testdemo
+fi
+
+docker build -t testdemo --build-arg env="Development" .
+docker run -d --restart=always -p 8022:80 --name testdemo testdemo
+docker cp /etc/localtime testdemo:/etc/
