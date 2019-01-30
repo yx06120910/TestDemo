2cf1fa1bd0960b46c2a93e3e7cddb1c5a643e18a
Amanda Yang <1756843703@qq.com>
2019-01-30 17:20:31 +0800

    傻吊

--- TestDemo/TestDemo/publish.sh
+++ TestDemo/TestDemo/publish.sh
@@ -8,6 +8,13 @@ docker container ls -a | grep "testdemo"
 #	docker network ls |grep testdemo
 #	docker network rm testdemo
 #fi
+if [ $? -eq 0 ];then
+    docker container stop testdemo
+    docker container rm testdemo
+    docker rmi  testdemo
+	docker network ls |grep testdemo
+	docker network rm testdemo
+fi
 
 docker build -t testdemo --build-arg env="Development" .
 docker run -d --restart=always -p 8023:80 --name testdemo testdemo
